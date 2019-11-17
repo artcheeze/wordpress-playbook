bcff174b105b6fa81bd5fefdca29f15b0776a2c9
Surasak <surasak.jaikum@mail.kmutt.ac.th>
2019-11-17 17:53:43 +0700

    Delete wp-config.php

--- wp-config.php
+++
@@ -1,9 +0,0 @@
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
-/** MySQL database username */
-define('DB_USER', 'ansible');
-/** MySQL database password */
-define('DB_PASSWORD', 'mypassword');
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
